4307fe090cfc4ddf13d475a8fbc907964a251eee
Machado <elitonmachadod200@gmail.com>
2021-08-25 17:47:02 -0300

    feat: check_id and define_var

--- src/jasmin.c
+++ src/jasmin.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include "jasmin.h"
 #include "../vector/vector.h"
 #include "../hashmap/hashmap.h"
 #include "../hashmap/hashmap_symbol.h"
 #include "../utils/str_utils.h"
+// #include "../output/verb.tab.h"
 
+int id_cont;
 hashmap* id_tab;
 vector* code_list;
 
 void jasmin_init() {
+	id_cont = 1;
     id_tab = hashmap_create(100005); 
     code_list = vector_create(); 
 }
@@ -38,8 +42,8 @@ void generate_header(char* source_file) {
 	write_code(".method public static main([Ljava/lang/String;)V");
 	write_code(".limit locals 100\n.limit stack 100");
 	/* generate temporal vars for syso*/
-	// defineVar("1syso_int_var",INT_T);
-	// defineVar("1syso_float_var",FLOAT_T);
+	define_var("1syso_int_var", INT_T);
+	define_var("1syso_float_var", FLOAT_T);
 	/*generate line*/
 	// write_line(1);
 }
@@ -55,4 +59,23 @@ void print_code(void) {
         fprintf(f, "%s\n", vector_get_char(code_list, i));
     }
     fclose(f);
+}
+
+bool check_id(char* id) {
+	return hashmap_has(id_tab, id);
+}
+
+void define_var(char* id, int type) {
+	if (check_id(id)) {
+        // yyerror(concat_many(3, "variable ", id, " already declared!"));
+	} else {
+		if (type == INT_T) {
+			write_code("iconst_0");
+			write_code(concat("istore ", i_to_str(id_cont)));
+		} else if (type == FLOAT_T) {
+			write_code("fconst_0");
+			write_code(concat("fstore ", i_to_str(id_cont)));
+		}
+		set_symbol(id_tab, id, id_cont++, type);
+	}
 }
--- src/jasmin.h
+++ src/jasmin.h
@@ -1,11 +1,14 @@
 #ifndef JASMIN_H
 #define JASMIN_H
 
+typedef enum {INT_T, FLOAT_T, BOOL_T, VOID_T, ERROR_T} type_enum;
+
 void jasmin_init();
 void jasmin_delete();
 void print_code(void);
 void generate_footer();
 void generate_header();
 void write_code(char *s);
-
+bool check_id(char* id);
+void define_var(char* id, int type);
 #endif
